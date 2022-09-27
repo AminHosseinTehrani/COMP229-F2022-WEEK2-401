b60be873543f335b596f5bdd0fbbff25a63c5ae0
Amin Tehrani <ahosse19@my.centennialcollege.ca>
2022-09-27 15:01:46 -0700

    changed app server

--- server.js
+++ server.js
@@ -1,51 +1,58 @@
-//third party modules
+import debug from 'debug';
+debug('comp-229');
+import http from 'http';
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import session from 'express-session';
+import app from './app/app.js';
 
-//
-// ES Modules fix for __dirname
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const PORT = normalizePort(process.env.PORT || 3000);
+app.set('port', PORT);
 
-//Import Routees
-import indexRouter from './app/routes/index.route.server.js'
+const server = http.createServer(app);
 
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
 
-//configuration module
-import {Secret} from './config/config.js';
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
 
+    if (port >= 0) {
+        return port;
+    }
 
+    return false;
+}
 
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-//Instantiate Express Application
-const app = express();
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
-//Set up Middlewares
-app.set('views',path.join(__dirname, '/app/views'))
-app.set('view engine', 'ejs');
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname,'/client')));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use(session({
-secret: Secret ,
-saveUninitialized: false,
-resave:false
-
-
-}));
-
-// app.use('/')
-app.use('/', indexRouter);
-
-app.listen(3000);
+function onListening() 
+{
+  let addr = server.address();
+  let bind = 'pipe ' + addr;
+  debug('Listening on ' + bind);
+}
